cf6dd61c27b3bfecacc2b095f9e8ec6754eb9e27
tinhlv <dxvantinh@gmail.com>
2018-06-11 23:19:25 +0700

    feedback 11-06

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,34 +3,29 @@ $(document).ready(function() {
   if (screensize  < 769) {
     $('.header-content .social').css('display', 'none');
     $("#mobile-menu-main").mmenu({
-       "extensions": [
-          "fx-menu-slide",
-          "theme-dark"
-       ]
+      "extensions": [
+        "fx-menu-slide",
+        "theme-dark",
+        "position-right",
+        "shadow-page",
+        "shadow-panels",
+        "pagedim-black",
+        "fx-listitems-slide",
+        "border-full",
+        "custom-width"
+      ],
+      wrappers: ["bootstrap4"],
+      "iconPanels": true
     });
   }
+  
   $('.selectpicker').selectpicker({
     style: 'btn-info',
     size: 4
   });
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId            : '308087493018290',
-      autoLogAppEvents : true,
-      xfbml            : true,
-      version          : 'v3.0'
-    });
-  };
-
-  (function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-  }(document, 'script', 'facebook-jssdk'));
 
   $('.birthday').bootstrapBirthday({
+    dateFormat: 'littleEndian',
     widget: {
       wrapper: {
         tag: 'div',
@@ -63,6 +58,59 @@ $(document).ready(function() {
         name: 'birthday[day]',
         class: 'form-control'
       }
+    },
+    text: {
+      year: "Year",
+      month: "Month",
+      day: "Day",
+      months: {
+        short: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+          "Oct",
+          "Nov",
+          "Dec"
+        ],
+        long: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December"
+        ]
+      }
     }
-  })
+  });
+  
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId            : '308087493018290',
+      autoLogAppEvents : true,
+      xfbml            : true,
+      version          : 'v3.0'
+    });
+  };
+
+  (function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
 });
